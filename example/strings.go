1c4ef81c30ec592ecdfc558c4b2d57073b995fe0
zhanbai <bai.zhan@qq.com>
2021-03-09 17:59:34 +0800

    feat:抽取子串

--- example/strings.go
+++ example/strings.go
@@ -13,6 +13,14 @@ func stringIndex() {
 	}
 }
 
+// 1.2 抽取子串
+func substr() {
+	fmt.Println("zhanbai"[0:4])
+	fmt.Println("watch out for that tree"[6:11])
+	fmt.Println("watch out for that tree"[17:])
+}
+
 func main() {
 	stringIndex()
+	substr()
 }
